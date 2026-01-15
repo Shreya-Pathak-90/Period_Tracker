979816ab381a340ccbc642383544211aaef1d127
midnightCompiler <pshreya906@gmail.com>
2026-01-15 19:44:58 +0000

    Required information for period tracking

---
+++ main.py
@@ -0,0 +1,22 @@
+class period:
+    def __init__(self):
+        print("The class is made")
+
+    def name_of_user(self, name:str)->str:
+        pass
+    def age(self,age:int)->int:
+        pass
+    def last_period_date(self,last_date:int)->int:
+        pass
+    def cycle_length(self,cycle_length:int)->int:
+        pass
+    def period_occur_days(self,duration:int)->int:
+        pass
+    def period_delayed(self,delayed:bool)->bool:
+        pass
+
+
+def main():
+    a = period()
+
+main() 
