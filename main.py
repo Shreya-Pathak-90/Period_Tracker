39295d9053bd39933015037f3ed61350d4c655a1
Shreya-Pathak-90 <pshreya906@gmail.com>
2026-01-16 18:53:34 +0000

    Getting the necessary information from the user

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 class period:
-    def __init__(self):
-        print("The class is made")
 
-    def name_of_user(self, name:str)->str:
-        pass
-    def age(self,age:int)->int:
-        pass
-    def last_period_date(self,last_date:int)->int:
-        pass
-    def cycle_length(self,cycle_length:int)->int:
-        pass
-    def period_occur_days(self,duration:int)->int:
-        pass
-    def period_delayed(self,delayed:bool)->bool:
-        pass
+    def name_of_user(self)->str:
+        name = input("Enter your name : ")
+        print(name)
+
+    def age(self)->int:
+        age = int(input("Enter your age :"))
+        print(age)
+
+    def last_period_date(self)->str:
+        last_date =input("What was your last period date (dd/mm/yyyy) : " )
+        print(last_date)
+
+    def cycle_length(self)->str:
+        cycle_length = input("What is your cycle length (---- days): " )
+        print(cycle_length)
+
+    def period_occur_days(self)->str:
+        duration = input("The number of days your period occurs (---- days): ")
+        print(duration)
+
+    def period_delayed(self)->bool:
+        delayed = input("Did you missed your period? ")
+        print(delayed)
 
 
 def main():
     a = period()
+    a.name_of_user()
+    a.age()
+    a.last_period_date()
+    a.cycle_length()
+    a.period_occur_days()
+    a.period_delayed()
+
 
 main() 
